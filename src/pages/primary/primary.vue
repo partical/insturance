e34b6b4f0af15064adae73d57967bff6a24e143c
liuh <liuh@succez.com>
2019-07-19 00:14:21 +0800

    提交首页卡片布局

--- src/pages/primary/primary.vue
+++ src/pages/primary/primary.vue
@@ -1,10 +1,10 @@
 <template>
 <div>
-	<el-row v-for="a in 2" :key="a">
-		<el-col v-for="(o, index) in 4" :key="o">
+	<el-row v-for="(a, i) in 2" :key="a">
+		<el-col v-for="(o, index) in 4" :key="o" :offset='index%4'>
 			<el-card>
-				<img :src="cards[index].path">
-				<span>{{cards[index].name}}</span>
+				<el-image :src="cards[i*4 + index].path"></el-image>
+				<span class='card__name'>{{cards[i*4 + index].name}}</span>
 			</el-card>
 		</el-col>
 	</el-row>
@@ -19,6 +19,8 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import vueImage from '../../assets/logo.png';
+
 @Component({})
 
 export default class PrimaryPage extends Vue{
@@ -26,11 +28,48 @@ export default class PrimaryPage extends Vue{
 	private data() {
 		return {
 			cards :[{
-				name: '',
-				path: ''
+				name: '儿童保险',
+				path: vueImage
+			}, {
+				name: '成人保险',
+				path: vueImage
+			}, {
+				name: '老人保险',
+				path: vueImage
+			}, {
+				name: '女性专区',
+				path: vueImage
+			},{
+				name: '方案设计',
+				path: vueImage
+			}, {
+				name: '大V解读',
+				path: vueImage
+			}, {
+				name: '其他保险',
+				path: vueImage
+			}, {
+				name: '理财专区',
+				path: vueImage
 			}],
 			carousels : []
 		}
 	}
 }
 </script>>
+
+
+<style>
+	.el-row {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.el-col {
+		flex-grow: 1;
+	}
+
+	.card__name {
+		display: block;
+	}
+</style>
