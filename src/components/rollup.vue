db59187bc0fcb34e979306ea0f1f3478ca9e1bc0
liuh <liuh@succez.com>
2019-07-21 23:23:18 +0800

    提交rollup组件

---
+++ src/components/rollup.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class = 'rollup'>
+		<div class = 'rollupitem' v-for="(item, index) in items" :key="index">{{item.name}}</div>
+	</div>
+</template>
+
+<script lang='ts' >
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import anime from 'animejs';
+
+interface RollupItemInfo {
+	name :string;
+	url :string;
+}
+
+
+/**
+ * 向上滚动组件。
+ */
+@Component
+export class Rollup extends Vue{
+
+	private items: RollupItemInfo[];
+
+	public mounted() {
+		
+	}
+}
+</script>
+
+<style>
+	.rollup {
+		height: 30px;
+	}
+
+	.rollupitem {
+		line-height: 30px;
+	}
+</style>
+ 
